719492480834f596c903e4e5f56cfd25ba7da6ba
Baptiste Leduc <baptiste@weglot.com>
2018-09-07 17:45:09 +0200

    if data is null, we run the command to fetch data again !

--- src/Controller/Controller.php
+++ src/Controller/Controller.php
@@ -5,9 +5,13 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use Psr\Cache\CacheItemPoolInterface;
+use Symfony\Bundle\FrameworkBundle\Console\Application;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller as BaseController;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Output\BufferedOutput;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\KernelInterface;
 
 class Controller extends BaseController
 {
@@ -19,14 +23,18 @@ class Controller extends BaseController
         'seasons'
     ];
 
+    /** @var KernelInterface */
+    protected $kernel;
+
     /** @var CacheItemPoolInterface */
     protected $cacheApp;
 
     /** @var string */
     protected $cachePrefix;
 
-    public function __construct(CacheItemPoolInterface $cacheApp, string $cachePrefix = 'ubisoftData')
+    public function __construct(KernelInterface $kernel, CacheItemPoolInterface $cacheApp, string $cachePrefix = 'ubisoftData')
     {
+        $this->kernel = $kernel;
         $this->cacheApp = $cacheApp;
         $this->cachePrefix = $cachePrefix;
     }
@@ -35,6 +43,21 @@ class Controller extends BaseController
     {
         if (in_array($data, static::FILES)) {
             $dataItem = $this->cacheApp->getItem($this->cachePrefix.ucfirst($data));
+
+            if (null === $dataItem->get()) {
+                $application = new Application($this->kernel);
+                $application->setAutoExit(false);
+
+                $input = new ArrayInput([
+                    'command' => 'crawler:fetch',
+                ]);
+                $output = new BufferedOutput();
+
+                $application->run($input, $output);
+                // $content = $output->fetch();
+
+                $dataItem = $this->cacheApp->getItem($this->cachePrefix.ucfirst($data));
+            }
             return new Response($dataItem->get());
         }
 
